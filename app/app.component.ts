4b6156e652518293400a61d9548d68a6b903403f
Moiz.Kachwala <moizk@hotmail.com>
2016-08-30 15:32:39 +0530

    styled bar chart

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-            <bar-graph bind-data="graphData" width="200" height="150" >
+            <bar-graph bind-data="graphData" width="230" height="170" >
             </bar-graph>
-            ` 
+            `
 })
 export class AppComponent {
   graphData: Array<number>;
   constructor() {
-    this.graphData = [10,20,30,40,60];
+    this.graphData = [10, 20, 30, 40, 60, 80, 90];
   }
 }
